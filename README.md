041d731389acf763e63566842dc5a6e244b8977c
Bede <bede.overend@gmail.com>
2016-03-10 00:01:03 +1100

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# rollup-plugin-file
+
+Basic rollup plugin to read a file (or vinyl) as an entry, rather than a string. Mainly for use with [gulp-process-inline](https://github.com/simplaio/gulp-process-inline)
---
+++ index.js
@@ -0,0 +1,29 @@
+var shortid = require('shortid');
+var path = require('path');
+
+module.exports = function() {
+  var paths = {};
+
+  return {
+    load: function(id) {
+      if (paths[id]) {
+        var file = paths[id];
+        return file.contents.toString();
+      }
+    },
+    resolveId: function(importee, importer) {
+      if (typeof importee !== 'string' && importee.contents) {
+        var id = 'file-' + shortid();
+        paths[id] = importee;
+        return id;
+      }
+
+      if (paths[importer]) {
+        var file = paths[importer],
+            requirePath = path.resolve(file.base, importee);
+
+        return require.resolve(requirePath);;
+      }
+    }
+  };
+};
